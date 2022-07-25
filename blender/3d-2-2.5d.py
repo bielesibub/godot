75b0f05f6034b400bc4c26b191f8685a36677804
bielesibub <paul@morphicventures.co.uk>
2022-07-25 18:25:12 +0100

    Update 3d-2-2.5d.py

--- blender/3d-2-2.5d.py
+++ blender/3d-2-2.5d.py
@@ -1,6 +1,13 @@
 # thanks -  https://clintbellanger.net/articles/isometric_tiles/
 #           https://www.youtube.com/watch?v=vIKJ1AqfI0Q
 
+# 3d to 2.5d blender script
+
+# add your model to render, with / without animation as a child to the RenderCollection.
+# it will render everything the camera sees to limit create a bounding box for the sprite (ctrl+b)
+# press 0 to see what the camera sees, then ctrl+b
+# if you want a border round the sprite use the freestyle and freestyle line thickness
+# disable antialiasing for good old fashioned 8/16-bit style
 
 import bpy
 from math import radians
@@ -19,7 +26,7 @@ platform = bpy.data.collections["RenderPlatform"]
 
 original_path = bpy.data.scenes[0].render.filepath
 
-for i in range(0,8):
+for i in range(0,1):
 
     # rotate the render platform and all children
     temp_rot = platform.objects["Armature"].rotation_euler
